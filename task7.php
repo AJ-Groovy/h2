ec03c7050530945b459d58750686226a3bd15b70
AJ <yaroslav01593@gmail.com>
2020-03-29 19:24:02 +0300

    task 7

---
+++ task7.php
@@ -0,0 +1,38 @@
+<?php
+
+class User
+{
+    private $name;
+    private $age;
+    private $email;
+
+    public function __call($name, $arguments)
+    {
+        if (!method_exists($this, $name)) {
+            echo $name . ' - Method does not exist <br>';
+        } else {
+            $this->$name(implode(',', $arguments));
+        }
+    }
+
+    private function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    private function setAge(int $age)
+    {
+        $this->age = $age;
+    }
+
+    public function getAll() {
+        return $this->name . ' - ' . $this->age . ' - ' . $this->email;
+    }
+}
+
+$usr = new User();
+
+$usr->setAge(12);
+$usr->setName('Teeeest');
+echo $usr->getAll();
+
